6c84ae72a7e1179a82ba6a853044dfeedd10cced
CrissCross <127163310+Cris-afk-svg@users.noreply.github.com>
2024-05-15 17:51:56 -0600

    Update db.ts
    Conexion SSL

--- src/db.ts
+++ src/db.ts
@@ -2,12 +2,15 @@ import {DataSource} from 'typeorm'
 import { User } from './entities/User'
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'postgres://typeormdb_user:8PPLoD8V7SWWNXnAx4ngrGFWol1FsPJK@dpg-cp2ju88l6cac73dg2nd0-a.oregon-postgres.render.com/typeormdb',
-    username: 'postgres',
+    host: 'dpg-cp2ju88l6cac73dg2nd0-a.oregon-postgres.render.com',
+    username: 'typeormdb_user',
     password: '8PPLoD8V7SWWNXnAx4ngrGFWol1FsPJK',
     port: 5432,
     database: 'typeormdb',
     entities: [User],
     logging: true,
-    synchronize: true
+    synchronize: true,
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
